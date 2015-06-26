1507e487b957ba59c05e9b22dacf9401df9f0442
dkn4 <dkn4@njit.edu>
2015-06-25 22:44:07 -0400

    Implemented initial version of Facebook Login. Needs modification.

---
+++ app/src/main/java/brymian/bubbles/damian/application/MainApplication.java
@@ -0,0 +1,42 @@
+package brymian.bubbles.damian.application;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Windows on 26-03-2015.
+ */
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+    }
+
+    /**
+     * Call this method inside onCreate once to get your hash key
+     */
+    public void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("BRYMIAN", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
