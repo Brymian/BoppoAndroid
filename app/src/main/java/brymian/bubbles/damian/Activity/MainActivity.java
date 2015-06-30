4d55c7b4fbbb601e79cdccd10257018fb2eaa4dd
dkn4 <dkn4@njit.edu>
2015-06-29 20:44:30 -0400

    Added a custom button for logging in through Bubbles.

--- app/src/main/java/brymian/bubbles/damian/Activity/MainActivity.java
+++ app/src/main/java/brymian/bubbles/damian/Activity/MainActivity.java
@@ -1,14 +1,13 @@
 package brymian.bubbles.damian.Activity;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import brymian.bubbles.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FragmentManager mFragmentManager;
+    // private FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
