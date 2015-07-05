994eed0f00ec939fa0c5690a450e42baaa330a6a
Project Wolf <bha4@njit.edu>
2015-07-05 06:20:23 -0400

    NEW API KEY THAT WORKS...make sure to clean/rebuild project to make key work

--- app/src/main/java/brymian/bubbles/bryant/MapsActivity.java
+++ app/src/main/java/brymian/bubbles/bryant/MapsActivity.java
@@ -12,12 +12,32 @@ import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import brymian.bubbles.R;
+/**
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps2);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
+**/
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
@@ -83,16 +103,7 @@ public class MapsActivity extends FragmentActivity {
 
         // Get latitude of the current location
         double latitude = myLocation.getLatitude();
-        /**
-         *
 
-        try{
-        double latitude = myLocation.getLatitude();
-        }
-        catch (NullPointerException e){
-            e.printStackTrace();
-        }
-**/
         // Get longitude of the current location
         double longitude = myLocation.getLongitude();
 
