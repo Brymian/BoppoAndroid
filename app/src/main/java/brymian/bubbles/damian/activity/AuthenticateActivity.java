ab32e4520ffd43d4e6e3ff991874b82bfd8152b2
dkn4 <dkn4@njit.edu>
2015-07-08 19:09:35 -0400

    Implemented fragment usage.

--- app/src/main/java/brymian/bubbles/damian/activity/AuthenticateActivity.java
+++ app/src/main/java/brymian/bubbles/damian/activity/AuthenticateActivity.java
@@ -1,9 +1,12 @@
 package brymian.bubbles.damian.activity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import brymian.bubbles.R;
+import brymian.bubbles.damian.fragment.Authenticate.LoginFragment;
 
 public class AuthenticateActivity extends AppCompatActivity {
 
@@ -11,10 +14,16 @@ public class AuthenticateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authenticate);
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.AuthenticateFragment, new LoginFragment());
+        ft.commit();
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
     }
+
 }
--- app/src/main/java/brymian/bubbles/damian/fragment/Authenticate/RegisterFragment.java
+++ app/src/main/java/brymian/bubbles/damian/fragment/Authenticate/RegisterFragment.java
@@ -4,33 +4,30 @@ import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Intent;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 
 import brymian.bubbles.R;
-import brymian.bubbles.bryant.MapsActivity;
 import brymian.bubbles.damian.nonactivity.GetUserCallback;
 import brymian.bubbles.damian.nonactivity.ServerRequests;
 import brymian.bubbles.damian.nonactivity.User;
-import brymian.bubbles.damian.nonactivity.UserStoreLocal;
 
 /**
  * Created by Ziomster on 7/2/2015.
  */
-public class LoginFragment extends Fragment implements View.OnClickListener {
+public class RegisterFragment extends Fragment implements View.OnClickListener {
 
-    Button bLogin;
+    Button bRegister;
     EditText etUsername, etPassword;
-    ImageButton ibRegisterLink;
-    UserStoreLocal userStoreLocal;
+    SharedPreferences sp;
 
-    public LoginFragment() {
+    public RegisterFragment() {
     }
 
     /*
@@ -44,17 +41,13 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_register, container, false);
 
         etUsername = (EditText) rootView.findViewById(R.id.etUsername);
         etPassword = (EditText) rootView.findViewById(R.id.etPassword);
-        bLogin = (Button) rootView.findViewById(R.id.bLogin);
-        ibRegisterLink = (ImageButton) rootView.findViewById(R.id.ibRegisterLink);
+        bRegister = (Button) rootView.findViewById(R.id.bRegister);
 
-        bLogin.setOnClickListener(this);
-        ibRegisterLink.setOnClickListener(this);
-
-        userStoreLocal = new UserStoreLocal(getActivity());
+        bRegister.setOnClickListener(this);
 
         return rootView;
     }
@@ -75,57 +68,45 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     /*
      * CUSTOM METHODS
      */
-
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.bLogin) {
+        if (v.getId() == R.id.bRegister) {
 
             String username = etUsername.getText().toString();
             String password = etPassword.getText().toString();
-
             User user = new User(username, password);
 
-            authenticate(user);
-
-        }
-        if (v.getId() == R.id.ibRegisterLink) {
-            // startActivity(new Intent(getActivity(), RegisterActivity.class));
-            FragmentManager fm = getActivity().getFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.AuthenticateFragment, new RegisterFragment());
-            ft.commit();
+            registerUser(user);
         }
     }
 
-    private void authenticate(User user) {
+    private void registerUser(User user) {
+
         ServerRequests serverRequest = new ServerRequests(getActivity());
-        serverRequest.getUserDataAsyncTask(user, new GetUserCallback() {
+        serverRequest.storeUserDataInBackground(user, new GetUserCallback() {
             @Override
             public void done(User returnedUser) {
-                if (returnedUser == null) {
-                    showErrorMessage();
-                } else {
-                    logUserIn(returnedUser);
-                }
+                showConfirmationMessage();
             }
         });
     }
 
-    private void showErrorMessage() {
+    private void showConfirmationMessage() {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
-        dialogBuilder.setMessage(("Incorrect user details."));
-        dialogBuilder.setPositiveButton("Ok", null);
+        dialogBuilder.setMessage(("Registration successful."));
+        dialogBuilder.setCancelable(false);
+        dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+                //startActivity(new Intent(getActivity(), LoginActivity.class));
+                FragmentManager fm = getActivity().getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.AuthenticateFragment, new LoginFragment());
+                ft.commit();
+            }
+        });
         dialogBuilder.show();
     }
-
-    private void logUserIn(User returnedUser) {
-        userStoreLocal.setUserData(returnedUser);
-        userStoreLocal.setUserLoggedStatus(true);
-
-        startActivity(new Intent(getActivity(), MapsActivity.class));
-
-    }
-
     /*
      * END OF CUSTOM METHODS
      */
