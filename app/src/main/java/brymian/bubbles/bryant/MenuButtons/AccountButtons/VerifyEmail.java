9381b050b649f4a6ac91e59cb8241bce50c607d9
Project Wolf <bha4@njit.edu>
2016-01-23 22:19:48 -0500

    verify email is complete...next is adding friendslist

--- app/src/main/java/brymian/bubbles/bryant/MenuButtons/AccountButtons/VerifyEmail.java
+++ app/src/main/java/brymian/bubbles/bryant/MenuButtons/AccountButtons/VerifyEmail.java
@@ -3,23 +3,46 @@ package brymian.bubbles.bryant.MenuButtons.AccountButtons;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import brymian.bubbles.R;
 import brymian.bubbles.bryant.MenuActivity;
+import brymian.bubbles.damian.nonactivity.ServerRequest;
+import brymian.bubbles.damian.nonactivity.StringCallback;
+import brymian.bubbles.damian.nonactivity.User;
+import brymian.bubbles.damian.nonactivity.UserDataLocal;
 
 
 public class VerifyEmail extends FragmentActivity implements View.OnClickListener{
     ImageButton ibMenu;
+    EditText etVerifyEmail;
+    Button bVerifyEmail;
+    int[] profileUserUID = new int[1];
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_account_changeemail);
+        setContentView(R.layout.verify_email);
 
         ibMenu = (ImageButton) findViewById(R.id.ibMenu);
+        etVerifyEmail = (EditText) findViewById(R.id.etVerifyEmail);
+        bVerifyEmail = (Button) findViewById(R.id.bVerifyEmail);
 
         ibMenu.setOnClickListener(this);
+        bVerifyEmail.setOnClickListener(this);
+
+        UserDataLocal udl = new UserDataLocal(this);
+        User userPhone = udl.getUserData();
+        int userUID = userPhone.getUid();
+        String userEmail = userPhone.getEmail();
+        System.out.println("getEmail(): " + userEmail);
+        etVerifyEmail.setText(userEmail);
+        setProfileUserUID(userUID);
     }
 
     public void onClick(View v){
@@ -28,6 +51,21 @@ public class VerifyEmail extends FragmentActivity implements View.OnClickListene
                 Intent menuIntent = new Intent(this, MenuActivity.class);
                 startActivity(menuIntent);
                 break;
+            case R.id.bVerifyEmail:
+                new ServerRequest(this).changeEmail(getProfileUserUID(), etVerifyEmail.getText().toString(), new StringCallback() {
+                    @Override
+                    public void done(String string) {
+                        Toast.makeText(VerifyEmail.this, string, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                break;
         }
+    }
+
+    void setProfileUserUID(int uid){
+        profileUserUID[0] = uid;
+    }
+    int getProfileUserUID(){
+        return profileUserUID[0];
     }
 }
