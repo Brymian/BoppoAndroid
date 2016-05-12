9bde0bb75403cf386761cb8a538f4063e633f049
almanza1112 <almanza1112@gmail.com>
2016-05-12 18:18:35 -0400

    MapsActivity updated

--- app/src/main/java/brymian/bubbles/bryant/map/MapsActivity.java
+++ app/src/main/java/brymian/bubbles/bryant/map/MapsActivity.java
@@ -1,6 +1,7 @@
 package brymian.bubbles.bryant.map;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -8,8 +9,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -21,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import brymian.bubbles.R;
+import brymian.bubbles.bryant.camera.CameraActivity;
 import brymian.bubbles.bryant.nonactivity.SaveSharedPreference;
 import brymian.bubbles.damian.nonactivity.ServerRequest.UserImageRequest;
 import brymian.bubbles.objects.Image;
@@ -41,7 +46,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.maps_activity);
 
         /*--------------------------------Checking for putExtras()--------------------------------*/
         String profile;
@@ -63,7 +68,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
         }
         /*----------------------------------------------------------------------------------------*/
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-
+        mToolbar.bringToFront();
         if(profile != null){
             if(profile.equals("logged in user")){
                 mToolbar.setTitle(R.string.My_Map);
@@ -71,6 +76,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
                 getUserImages(SaveSharedPreference.getUserUID(this));
 
             }else if(profile.equals("all")){
+                mToolbar.setTitle(R.string.Explore);
                 getAllImages();
             }
             else{
@@ -84,6 +90,24 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        FloatingActionMenu famMenu = (FloatingActionMenu) findViewById(R.id.famMenu);
+        famMenu.showMenuButton(true);
+
+        FloatingActionButton fabCamera = (FloatingActionButton) findViewById(R.id.fabCamera);
+        fabCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MapsActivity.this, CameraActivity.class).putExtra("imagePurpose", "Regular"));
+            }
+        });
+
+        FloatingActionButton fabFilter = (FloatingActionButton) findViewById(R.id.fabFilter);
+        fabFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 
