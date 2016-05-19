db85b56707b463b8504e62aae7223f49069699f1
almanza1112 <almanza1112@gmail.com>
2016-05-19 05:11:23 -0400

    EpisodeMy updated

--- app/src/main/java/brymian/bubbles/bryant/episodes/EpisodeAttendingRecyclerAdapter.java
+++ app/src/main/java/brymian/bubbles/bryant/episodes/EpisodeAttendingRecyclerAdapter.java
@@ -9,8 +9,6 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
 
 import brymian.bubbles.R;
