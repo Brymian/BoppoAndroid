585ae1b6f4eff3960ec6b71d87eeecee058fe71e
Ziomster <ziomster44@gmail.com>
2016-05-18 20:19:56 -0400

    Committing merged changes.

--- app/src/main/java/brymian/bubbles/bryant/pictures/ProfilePicturesActivity2.java
+++ app/src/main/java/brymian/bubbles/bryant/pictures/ProfilePicturesActivity2.java
@@ -7,7 +7,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -48,7 +47,7 @@ public class ProfilePicturesActivity2 extends AppCompatActivity implements View.
 
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_pictures_activity2);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
