0df95dce008adbcaafb56d9dc1480607cc75b913
almanza1112 <almanza1112@gmail.com>
2017-04-17 17:53:03 -0400

    big ass update

--- app/src/main/java/brymian/bubbles/bryant/logIn/LoginActivity.java
+++ app/src/main/java/brymian/bubbles/bryant/logIn/LoginActivity.java
@@ -5,7 +5,6 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
