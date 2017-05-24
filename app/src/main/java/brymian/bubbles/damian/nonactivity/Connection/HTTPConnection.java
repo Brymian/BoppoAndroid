6cd86f4bc99ba4f3754c97ec3f0b03312d32b78e
almanza1112 <almanza1112@gmail.com>
2017-05-24 10:47:54 -0400

    changing port to new pi

--- app/src/main/java/brymian/bubbles/damian/nonactivity/Connection/HTTPConnection.java
+++ app/src/main/java/brymian/bubbles/damian/nonactivity/Connection/HTTPConnection.java
@@ -6,8 +6,8 @@ public class HTTPConnection {
     private final String PROTOCOL = "http";
     //private final String SERVER   = "192.168.0.13";
     private final String SERVER  = "173.70.18.187";
-    private final int    PORT = 8080;
-    //private final int    PORT     = 1311;
+    //private final int    PORT = 8080;
+    private final int    PORT     = 1311;
     private final String UPLOADS  = "Bubbles/Uploads/";
     private final String PHP      = "BubblesServer/";
 
